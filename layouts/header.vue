cb81a6e969a6907b6a952748304e9ca0a1d1bf5d
SoominYim <soo15soo@kakao.com>
2024-07-16 09:53:13 +0900

    feat : dir update

--- layouts/header.vue
+++ layouts/header.vue
@@ -1,5 +1,5 @@
-<template lang="">
+<template>
   <div></div>
 </template>
 <script lang="ts" setup></script>
-<style lang=""></style>
+<style lang="scss"></style>
---
+++ server/api/index.ts
@@ -0,0 +1,16 @@
+// const express = require("express");
+// const app = express();
+// // 미들웨어이기 때문에 port는 따로 지정하지 않아도 되는 것 같다.
+
+// // route를 직접 작성할 경우
+// app.get("/", function (req: any, res: any) {
+//   res.send("Hello World!");
+// });
+
+// // route 설정을 분리할 경우
+// // 물론 여럿 추가할 수 있음
+
+// module.exports = {
+//   path: "/api",
+//   handler: app,
+// };
