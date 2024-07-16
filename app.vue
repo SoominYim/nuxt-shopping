d3b5708f097bbc2be156baf2f0ae503ece0a9693
SoominYim <soo15soo@kakao.com>
2024-07-16 12:27:04 +0900

    style : header layout update

--- app.vue
+++ app.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<style>
+  @import url("@/assets/style/reset.scss");
+  #__nuxt {
+    border-left: 1px solid rgba(138, 138, 138, 0.3);
+    border-right: 1px solid rgba(138, 138, 138, 0.3);
+    max-width: 600px;
+
+    height: auto;
+    margin: 0 auto;
+  }
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <header id="main__header">
+    <div class="header__layout">
+      <div class="notice__wrap">
+        <button class="main__notice">알림</button>
+      </div>
+      <div class="logo__wrap">
+        <span class="main__logo">로고</span>
+      </div>
+      <div class="sub__wrap">
+        <button class="main__search">검색</button>
+        <button class="main__cart">장바구니</button>
+      </div>
+    </div>
+  </header>
+</template>
+<script lang="ts" setup></script>
+<style lang="scss">
+  #main__header {
+    .header__layout {
+      width: 100%;
+      height: 50px;
+      padding: 0 10px;
+      display: flex;
+      justify-content: space-between;
+    }
+    .header__layout > div {
+      width: auto;
+      height: auto;
+      display: flex;
+      -webkit-box-align: center;
+      align-items: center;
+      flex-basis: 80px;
+      flex-shrink: 0;
+      button {
+        width: 40px;
+      }
+      span {
+        text-align: center;
+        width: 100px;
+      }
+    }
+  }
+</style>
