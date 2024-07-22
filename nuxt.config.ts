31f0e5d2a6d780bd0a0261dabd7789619dea295c
SoominYim <soo15soo@kakao.com>
2024-07-22 16:16:02 +0900

    feat : 모바일 확대 방지

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     head: {
+      viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
       script: [{ src: "https://developers.kakao.com/sdk/js/kakao.min.js" }],
     },
   },
